264f43b7b4c1fa50254ebd1a808fd1b6c23c78fa
rmacedo88 <robson.alves.macedo@gmail.com>
2021-06-15 01:47:34 -0300

    Acionamento parcial dos testes unitários no script integrado ao Makefile

--- scripts/test_unit.sh
+++ scripts/test_unit.sh
@@ -1,1 +1,16 @@
-echo "Teste"
+echo "
+"
+
+echo "  TESTANDO Main/Main.go"
+echo "  test case: configurarTamanhoHashCommit"
+go tool test2json -t /tmp/___Main_test_go.test -test.v -test.paniconexit0 -test.run Test_configurarTamanhoHashCommit
+echo "  test case: main"
+go tool test2json -t /tmp/___Main_test_go.test -test.v -test.paniconexit0 -test.run Test_main
+echo "
+"
+
+echo "  TESTANDO GitLog/LinhaLog.go"
+echo "  test case: buildLinhaLog"
+go tool test2json -t /tmp/___Test_buildLinhaLog_in_bb_captura_artefatos_ordem_fornecimento_GitLog.test -test.v -test.paniconexit0 -test.run Test_buildLinhaLog
+echo "
+"
